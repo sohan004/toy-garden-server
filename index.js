bbd6685ef2ee95ea35bb4085b1b5e4f63259b486
sohan004 <aryansohan02@gmail.com>
2023-05-20 01:01:04 +0600

    single data information get done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.Mongo_Id}:${process.env.Mongo_Pass}@cluster0.bitxn0d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,10 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const database = client.db('toysDB').collection('toys')
+        const indexKeys = { toyName: 1 }
+        const indexOptions = { name: 'toy_name' }
+        const index = await database.createIndex(indexKeys, indexOptions)
+
         app.post('/toys', async (req, res) => {
             const body = req.body
             const result = await database.insertOne(body);
@@ -34,6 +38,43 @@ async function run() {
             const result = database.find().limit(20)
             const toArray = await result.toArray()
             res.send(toArray)
+        })
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id
+            const quary = { _id: new ObjectId(id) }
+            const delet = await database.deleteOne(quary)
+            res.send(delet)
+        })
+        app.get('/toys/:text', async (req, res) => {
+            const text = req.params.text
+            const result = await database.find({ toyName: { $regex: text, $options: "i" } }).limit(20)
+            const toArray = await result.toArray()
+            res.send(toArray)
+        })
+        app.get('/my_toys', async (req, res) => {
+            const { email } = req.query
+            const data = await database.find({ email: { $eq: email } })
+            const toArray = await data.toArray()
+            res.send(toArray)
+
+        })
+        app.put('/toys/:id', async (req, res) => {
+            const id = req.params.id
+            const quary = { _id: new ObjectId(id) }
+            const body = req.body
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: body
+            };
+            const result = await database.updateOne(quary, updateDoc, options)
+            res.send(result)
+
+        })
+        app.get('/toy_details/:id', async (req, res) => {
+            const id = req.params.id
+            const quary = { _id: new ObjectId(id) }
+            const result = await database.findOne(quary)
+            res.send(result)
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
